338002b162c8423c079df5ecacc0b8e2d88976ef
khudaynazrovs <khudaynazrovsd@gmail.com>
2024-04-13 20:00:50 +0900

    modify

--- src/constants/footer/footerstyle.js
+++ src/constants/footer/footerstyle.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 export const Footerwrapper=styled.div`
 display: flex;
+justify-content: space-around;
 
 `
 export const Camper =styled.div`
+display: flex;
 color: #FFF;
 font-family: sans-serif;
 font-size: 40px;
@@ -12,8 +14,32 @@ font-weight: 700;
 line-height: normal;
 `
 export const Service =styled.div`
+display: flex;
+flex-direction: column;
+color: #FFF;
+font-family: Montserrat;
+font-size: 20px;
+font-style: normal;
+font-weight: 600;
+line-height: normal;
 `
 export const Menu =styled.div`
+display: flex;
+flex-direction: column;
+color: #FFF;
+font-family: Montserrat;
+font-size: 20px;
+font-style: normal;
+font-weight: 600;
+line-height: normal;
 `
 export const Contact =styled.div`
+display: flex;
+flex-direction: column;
+color: #FFF;
+font-family: Montserrat;
+font-size: 20px;
+font-style: normal;
+font-weight: 600;
+line-height: normal;
 `
